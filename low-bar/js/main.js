d9cd8928b24bc712f5a066b8213f2345adf9ef92
Pablo Renero Balgañón <prenero4bc@gamil.com>
2022-01-10 02:41:27 +0100

    feat: horizontally animatec nav bar

---
+++ low-bar/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function itemClick() {
+    const list = document.querySelectorAll(".navbar__item");
+    list.forEach((item) => item.classList.remove("active"));
+    this.classList.add("active");
+}
+window.onload = () => {
+    const list = document.querySelectorAll(".navbar__item");
+    list.forEach((item) => item.addEventListener("click", itemClick));
+};
+//# sourceMappingURL=main.js.map
